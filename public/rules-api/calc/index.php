d691a7372b6bbfc5d7def2fd14e174adc1e84902
Stuart Pringle <kaboodleschmitt@hotmail.com>
2026-02-08 04:59:38 -0500

    fix(calc): unify cors headers

--- public/rules-api/calc/index.php
+++ public/rules-api/calc/index.php
@@ -3,9 +3,6 @@
 declare(strict_types=1);
 
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
   http_response_code(204);
